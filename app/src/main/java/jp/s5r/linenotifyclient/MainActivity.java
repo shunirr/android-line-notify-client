77d6bcaf5a9292f190a7d438f67ef4361971eebc
shunirr <m@s5r.jp>
2016-11-28 22:11:26 +0900

    Add test button.

--- app/src/main/java/jp/s5r/linenotifyclient/MainActivity.java
+++ app/src/main/java/jp/s5r/linenotifyclient/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.s5r.linenotifyclient;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 public class MainActivity extends PreferenceActivity {
@@ -8,5 +10,17 @@ public class MainActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings_prefs);
+
+        Preference testPrefs = findPreference(getString(R.string.prefs_test));
+        testPrefs.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(MainActivity.this, SendActivity.class);
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, "Test Message");
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 }
